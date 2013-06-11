af1f9775b2d1dbfd6dba83e8a914aaccd82a8706
Xin Wang <dram.wang@gmail.com>
2013-06-11 14:03:45 +0800

    Generate ref doc using _ref/arc.

--- _ref/generate.sh
+++ _ref/generate.sh
@@ -1,1 +1,18 @@
-racket -f ../../as.scm generate.arc
+#!/bin/sh
+#|
+exec racket -f $0
+|#
+
+(require racket/base)
+
+(require "arc/ac.scm") 
+(require "arc/brackets.scm")
+(use-bracket-readtable)
+
+(parameterize ((current-directory "arc"))
+  (aload "arc.arc")
+  (aload "libs.arc"))
+
+(aload "generate.arc")
+
+(system "cp output/* ../ref/")
