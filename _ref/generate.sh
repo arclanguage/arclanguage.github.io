6453e6e2323379ce637b3d6f5af6abeb954c9468
Kartik K. Agaram <vc@akkartik.com>
2014-04-30 09:08:32 -0700

    streamline generator

--- _ref/generate.sh
+++ _ref/generate.sh
@@ -5,7 +5,7 @@ exec racket -f $0
 
 (require racket/base)
 
-(require "arc/ac.scm") 
+(require "arc/ac.scm")
 (require "arc/brackets.scm")
 (use-bracket-readtable)
 
@@ -13,6 +13,15 @@ exec racket -f $0
   (aload "arc.arc")
   (aload "libs.arc"))
 
-(aload "generate.arc")
-
-(system "cp output/* ../ref/")
+(system "rm -rf html")
+(arc-eval '(do
+  (ensure-dir "html")
+  (declare 'atstrings nil)
+  (load "template.arc")
+  (runall)
+  ;fails with "Expected tests" error
+  ;(run "foundation-doc.tem")
+  (system "cp foundation-doc.html html/")
+))
+(system "cp docs/* html/* ../ref")
+(system "rm -rf html")
