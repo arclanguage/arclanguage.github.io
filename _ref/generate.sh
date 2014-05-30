16a4081a1ef3fc3946a8826d75ea869b19c6d3b3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-05-31 01:32:26 +0400

    Script generations was changed to build docset for Dash if sqlite3 is found.

--- _ref/generate.sh
+++ _ref/generate.sh
@@ -14,7 +14,14 @@ exec racket -f $0
   (aload "libs.arc"))
 
 (system "rm -rf html")
+
+(system "rm -rf inserts.sql")
+
 (arc-eval '(do
+  (w/outfile sql "inserts.sql"
+    (w/stdout sql
+      (prn "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")
+      (prn "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);")))
   (ensure-dir "html")
   (declare 'atstrings nil)
   (load "template.arc")
@@ -22,3 +29,12 @@ exec racket -f $0
   (run "foundation-doc.tem")))
 (system "cp docs/* html/* ../ref")
 (system "rm -rf html")
+
+(system "rm -fr ../arc.docset/Contents/Resources/Documents/html")
+(system "cp -r ../ref ../arc.docset/Contents/Resources/Documents/html")
+;; cut navigation toolbar because it is unnecessary in Dash
+(system "sed -i '' '/navbar-inverse/,/end of navbar/d' ../arc.docset/Contents/Resources/Documents/html/*.html")
+
+(system "rm -f ../arc.docset/Contents/Resources/docSet.dsidx")
+(system "which sqlite3 > /dev/null && cat inserts.sql | sqlite3 ../arc.docset/Contents/Resources/docSet.dsidx")
+(system "rm -f inserts.sql")
