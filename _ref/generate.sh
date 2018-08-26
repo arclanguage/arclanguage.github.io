feeb7545fcd1b5d9583b260a14c4cccbd152340e
Ross Angle <rokitna@hotmail.com>
2018-08-26 00:28:49 -0700

    Factor the Dash build out to its own section in generate.sh.

--- _ref/generate.sh
+++ _ref/generate.sh
@@ -13,22 +13,35 @@ exec racket -f $0
   (aload "arc.arc")
   (aload "libs.arc"))
 
+(define-syntax-rule (arc-do body ...)
+  (arc-eval '(do body ...)))
+
+
+; Build the ref/ directory. It will be served by GitHub pages.
+
 (system "rm -rf html")
 
-(system "rm -rf inserts.sql")
-
-(arc-eval '(do
-  (w/outfile sql "inserts.sql"
-    (w/stdout sql
-      (prn "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")
-      (prn "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);")))
+(arc-do
   (ensure-dir "html")
   (declare 'atstrings nil)
   (load "template.arc")
   (runall)
-  (run "foundation-doc.tem")))
+  (run "foundation-doc.tem"))
 (system "cp docs/* html/* ../ref")
 (system "rm -rf html")
+
+
+; Based on the ref/ directory we just built, build an arc.docset/
+; directory in a format Dash can browse as offline documentation
+; (https://kapeli.com/dash).
+
+(system "rm -rf inserts.sql")
+
+(arc-do
+  (w/outfile sql "inserts.sql"
+    (w/stdout sql
+      (prn "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")
+      (prn "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);"))))
 
 (system "rm -fr ../arc.docset/Contents/Resources/Documents/html")
 (system "cp -r ../ref ../arc.docset/Contents/Resources/Documents/html")
